6fe06f83d84e9de0b4ce93cd8def5bc3a48d9dfa
abhinavsingh649 <abhinavsingh649@gmail.com>
2023-03-02 23:38:47 +0530

    Initial commit

---
+++ Backend/Soul_Boggling/src/main/java/com/abhinav/Soul_Boggling/SoulBogglingApplication.java
@@ -0,0 +1,13 @@
+package com.abhinav.Soul_Boggling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoulBogglingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoulBogglingApplication.class, args);
+	}
+
+}
---
+++ Backend/Soul_Boggling/src/test/java/com/abhinav/Soul_Boggling/SoulBogglingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhinav.Soul_Boggling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoulBogglingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
